1836f15959a96772a7c7d710e60e71a91ebaf048
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-20 13:59:56 -0400

    4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-export async function start(){
+async function start(){
         const response = await fetch 
         ("https://dog.ceo/api/breeds/list/all")
         const data = await response.json()
@@ -9,13 +9,22 @@ start()
 
 function createBreedList(breedList){
 document.getElementById("breed").innerHTML = `
-<select onchange="">
+<select onchange="loadByBreed(this.value)">
                 <option>Choose A Dog Breed</option>
                ${Object.keys(breedList).map(function(breed){
                 return `<option>${breed}</option>`
                }).join("")}
             </select>
 `
+}
+
+async function loadByBreed(breed){
+  if(breed != "Choose A Dog Breed"){
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    console.log(data)
+  }
+  
 }
 
   
